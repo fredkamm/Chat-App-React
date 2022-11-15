feee05efb8217308183a57a7dc51393523ab01b8
fredkamm <fred.kamm95@gmail.com>
2022-11-15 11:03:08 -0500

    firebase instal/config

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-gray-800 h-20 flex justify-between items-center p-4'>
+      <h1 className='text-white text-3xl'>Chat App</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ_CCfLcErOZCeVrbUP69ECEfoEk6RDIw",
+  authDomain: "chat-app-a9849.firebaseapp.com",
+  projectId: "chat-app-a9849",
+  storageBucket: "chat-app-a9849.appspot.com",
+  messagingSenderId: "28714493615",
+  appId: "1:28714493615:web:a62a8cbf1e84b3d4fa565a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
